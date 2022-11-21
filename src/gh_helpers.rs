dd12cf27f43a467f29b085337fa05ccf2f9f6102
yonchicy <1079279184@qq.com>
2022-11-21 21:56:39 +0800

    add a function to get user name

--- src/gh_helpers.rs
+++ src/gh_helpers.rs
@@ -1,10 +1,38 @@
 use std::process::{Command, Stdio};
-pub fn create_respo(repo_name:&str,is_public_repo:bool) {
+fn create_repos(repo_name:&str,is_public_repo:bool) {
     let private_flag = if is_public_repo { "--public"} else {"--private"};
-    let output = Command::new("gh")
+     Command::new("gh")
         .arg("repo")
         .arg("create")
         .arg(repo_name)
         .arg(private_flag);
 
+}
+
+fn get_user_name() -> String{
+
+    let child = Command::new("git").arg("config").arg("--list").output().expect("failed to run cmd");
+
+
+    let output =  String::from_utf8( child.stdout).expect("error");
+
+    for line in output.lines() {
+        if line.contains("user.name="){
+            let (head,name) = line.split_at(10);
+            return name.to_string();
+        }
+    }
+    return "".to_string();
+    
+}
+
+fn link_repos(){
+    let user_name = get_user_name();
+    let remote_link  = format!("git@github.com:{}/",user_name);
+    Command::new("git")
+    .arg("remote")
+    .arg("add")
+    .arg("orgin")
+    .arg(remote_link);
+    
 }
